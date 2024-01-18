4b25de29f9f0491cb79c3492a8d66a7d22f45a4d
praveen <praveenpadigapati70@gmail.com>
2024-01-18 11:07:57 +0530

    first cmit

---
+++ newone/src/newone/first.java
@@ -0,0 +1,17 @@
+package newone;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+		
+		String eat="banana";
+		if(eat.equals("banana")) {
+			System.out.println("full stumac");
+		}
+		if(eat.equalsIgnoreCase("ba")) {
+			System.out.println("full stumac");
+		}
+	}
+
+}
